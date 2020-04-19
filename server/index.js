5e68c27b62a880f836cdd00f2371503bf0c49eba
aliaksandr-afk <aleks.feedback@gmail.com>
2020-04-19 18:34:47 +0300

    feat: get right settings and libraries babel

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
-import unirest from "unirest";
-import cheerio from "cheerio";
-// import parsePost from "../parsePost.js";
+// import unirest from "unirest";
+// import cheerio from "cheerio";
+import parsePost from "../parsePost.js";
 
-function parsePost(url, titleClass, imageClass, imageAttr, textClass, commentsClass, isSlice) {
-    unirest.get(url).end(function(response) {
-        const body = response.body;
-        const $ = cheerio.load(body);
-        const title = $(titleClass).text();
-        if(isSlice) {
-            var image = $(imageClass).attr(imageAttr).slice(22, -2);
-        } else {
-            var image = $(imageClass).attr(imageAttr);
-        };
-        const text = $(textClass).text();
-        const comments = $(commentsClass).text().trim();
+// function parsePost(url, titleClass, imageClass, imageAttr, textClass, commentsClass, isSlice) {
+//     unirest.get(url).end(function(response) {
+//         const body = response.body;
+//         const $ = cheerio.load(body);
+//         const title = $(titleClass).text();
+//         if(isSlice) {
+//             var image = $(imageClass).attr(imageAttr).slice(22, -2);
+//         } else {
+//             var image = $(imageClass).attr(imageAttr);
+//         };
+//         const text = $(textClass).text();
+//         const comments = $(commentsClass).text().trim();
     
-        const post = {
-            title: title,
-            image: image,
-            text: text,
-            comments: comments
-        }
-        console.log(post);
-    })
-}
+//         const post = {
+//             title: title,
+//             image: image,
+//             text: text,
+//             comments: comments
+//         }
+//         console.log(post);
+//     })
+// }
 
 parsePost(
     'https://news.tut.by/society/680939.html',
